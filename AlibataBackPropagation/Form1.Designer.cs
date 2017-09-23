7971947184c4263ef0fbcfca420fd1cd7d9f79d0
jamesnogra <love.gerald@yahoo.com>
2017-09-23 22:45:06 +0800

    Almost done.

--- AlibataBackPropagation/Form1.Designer.cs
+++ AlibataBackPropagation/Form1.Designer.cs
@@ -30,7 +30,17 @@
         {
             this.drawer = new System.Windows.Forms.PictureBox();
             this.clearDrawing = new System.Windows.Forms.Button();
+            this.train = new System.Windows.Forms.Button();
+            this.trainProgressBar = new System.Windows.Forms.ProgressBar();
+            this.test = new System.Windows.Forms.Button();
+            this.legend = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.resultPicBox = new System.Windows.Forms.PictureBox();
+            this.resultLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.drawer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.legend)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPicBox)).BeginInit();
             this.SuspendLayout();
             // 
             // drawer
@@ -39,6 +49,7 @@
             this.drawer.Location = new System.Drawing.Point(13, 13);
             this.drawer.Name = "drawer";
             this.drawer.Size = new System.Drawing.Size(320, 320);
+            this.drawer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.drawer.TabIndex = 0;
             this.drawer.TabStop = false;
             this.drawer.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawer_MouseDown);
@@ -50,26 +61,108 @@
             this.clearDrawing.BackColor = System.Drawing.Color.IndianRed;
             this.clearDrawing.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.clearDrawing.ForeColor = System.Drawing.Color.White;
-            this.clearDrawing.Location = new System.Drawing.Point(340, 13);
+            this.clearDrawing.Location = new System.Drawing.Point(339, 13);
             this.clearDrawing.Name = "clearDrawing";
-            this.clearDrawing.Size = new System.Drawing.Size(274, 35);
+            this.clearDrawing.Size = new System.Drawing.Size(362, 35);
             this.clearDrawing.TabIndex = 1;
             this.clearDrawing.Text = "Clear Drawing";
             this.clearDrawing.UseVisualStyleBackColor = false;
             this.clearDrawing.Click += new System.EventHandler(this.clearDrawing_Click);
             // 
+            // train
+            // 
+            this.train.BackColor = System.Drawing.Color.SteelBlue;
+            this.train.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.train.ForeColor = System.Drawing.Color.White;
+            this.train.Location = new System.Drawing.Point(339, 55);
+            this.train.Name = "train";
+            this.train.Size = new System.Drawing.Size(362, 35);
+            this.train.TabIndex = 2;
+            this.train.Text = "Train";
+            this.train.UseVisualStyleBackColor = false;
+            this.train.Click += new System.EventHandler(this.train_Click);
+            // 
+            // trainProgressBar
+            // 
+            this.trainProgressBar.Location = new System.Drawing.Point(340, 55);
+            this.trainProgressBar.Name = "trainProgressBar";
+            this.trainProgressBar.Size = new System.Drawing.Size(361, 35);
+            this.trainProgressBar.TabIndex = 3;
+            // 
+            // test
+            // 
+            this.test.BackColor = System.Drawing.Color.SeaGreen;
+            this.test.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.test.ForeColor = System.Drawing.Color.White;
+            this.test.Location = new System.Drawing.Point(339, 96);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(362, 35);
+            this.test.TabIndex = 4;
+            this.test.Text = "Test";
+            this.test.UseVisualStyleBackColor = false;
+            this.test.Click += new System.EventHandler(this.test_Click);
+            // 
+            // legend
+            // 
+            this.legend.Location = new System.Drawing.Point(441, 140);
+            this.legend.Name = "legend";
+            this.legend.Size = new System.Drawing.Size(159, 87);
+            this.legend.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.legend.TabIndex = 5;
+            this.legend.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.resultLabel);
+            this.groupBox1.Controls.Add(this.resultPicBox);
+            this.groupBox1.Location = new System.Drawing.Point(340, 233);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(361, 100);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Result";
+            // 
+            // resultPicBox
+            // 
+            this.resultPicBox.BackColor = System.Drawing.Color.White;
+            this.resultPicBox.Location = new System.Drawing.Point(7, 17);
+            this.resultPicBox.Name = "resultPicBox";
+            this.resultPicBox.Size = new System.Drawing.Size(59, 74);
+            this.resultPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.resultPicBox.TabIndex = 0;
+            this.resultPicBox.TabStop = false;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(70, 43);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(288, 18);
+            this.resultLabel.TabIndex = 1;
+            this.resultLabel.Text = "0.0000 0.0000 0.0000 0.0000 ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(620, 347);
+            this.ClientSize = new System.Drawing.Size(713, 347);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.legend);
+            this.Controls.Add(this.test);
             this.Controls.Add(this.clearDrawing);
             this.Controls.Add(this.drawer);
+            this.Controls.Add(this.train);
+            this.Controls.Add(this.trainProgressBar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Alibata Baybayin Backpropagation";
             ((System.ComponentModel.ISupportInitialize)(this.drawer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.legend)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPicBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -78,6 +171,13 @@
 
         private System.Windows.Forms.PictureBox drawer;
         private System.Windows.Forms.Button clearDrawing;
+        private System.Windows.Forms.Button train;
+        private System.Windows.Forms.ProgressBar trainProgressBar;
+        private System.Windows.Forms.Button test;
+        private System.Windows.Forms.PictureBox legend;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.PictureBox resultPicBox;
     }
 }
 
--- AlibataBackPropagation/Form1.cs
+++ AlibataBackPropagation/Form1.cs
@@ -8,20 +8,179 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Backprop;
+using System.Drawing.Imaging;
+using System.Threading;
 
 namespace AlibataBackPropagation
 {
     public partial class Form1 : Form
     {
+        NeuralNet bp;
         Bitmap drawerBitmap;
+        int totalEpochs = 20000;
+        char[] allCharaters = new char[3] { 'A', 'E', 'O' };
+        byte[,,] allBinaryImages = new byte[3, 30, 64];
+        byte[] inputImage = new byte[64];
         bool paintFlag = false;
-        byte brushWidth = 20;
+        byte brushWidth = 25;
 
         public Form1()
         {
             InitializeComponent();
             drawerBitmap = new Bitmap(drawer.Width, drawer.Height);
             drawer.Image = drawerBitmap;
+            legend.Image = (Bitmap)Image.FromFile(@"legend.jpg");
+            bp = new NeuralNet(64, 4, 4);
+        }
+
+        private void train_Click(object sender, EventArgs e)
+        {
+            train.Visible = false;
+            trainProgressBar.Value = 0;
+
+            getImagesAndStoreBinaryData();
+            beginTraining();
+
+            MessageBox.Show("Finished training.");
+            train.Visible = true;
+        }
+
+        private void beginTraining()
+        {
+            decimal totalPercent;
+            for (int x=0; x<totalEpochs; x++)
+            {
+                for (int i=0; i<allBinaryImages.GetLength(0); i++)
+                {
+                    for (int j=0; j<allBinaryImages.GetLength(1); j++)
+                    {
+                        for (int k=0; k<allBinaryImages.GetLength(2); k++)
+                        {
+                            bp.setInputs(k, (double)allBinaryImages[i, j, k]);
+                        }
+                        if (i==0) //for A is 0000
+                        {
+                            bp.setDesiredOutput(0, (double)0);
+                            bp.setDesiredOutput(1, (double)0);
+                            bp.setDesiredOutput(2, (double)0);
+                            bp.setDesiredOutput(3, (double)1);
+                        }
+                        else if (i == 1) //for E is 0001
+                        {
+                            bp.setDesiredOutput(0, (double)0);
+                            bp.setDesiredOutput(1, (double)0);
+                            bp.setDesiredOutput(2, (double)1);
+                            bp.setDesiredOutput(3, (double)0);
+                        }
+                        else if (i == 2) //for O is 0010
+                        {
+                            bp.setDesiredOutput(0, (double)0);
+                            bp.setDesiredOutput(1, (double)0);
+                            bp.setDesiredOutput(2, (double)1);
+                            bp.setDesiredOutput(3, (double)1);
+                        }
+                        bp.learn();
+                    }
+                }
+                Application.DoEvents();
+                totalPercent = (decimal)((decimal)(x+1) / (decimal)totalEpochs * (decimal)100);
+                trainProgressBar.Value = (int)totalPercent;
+                //MessageBox.Show("Val: " + x + " and " + totalEpochs + "\ndiv: "+ totalPercent);
+            }
+        }
+
+        private void getImagesAndStoreBinaryData()
+        {
+            Bitmap tempImage;
+            int tempImageCounter;
+            string tempStr;
+
+            for (int i=0; i<allCharaters.Length; i++)
+            {
+                for (int x=0; x<10; x++)
+                {
+                    tempImage = (Bitmap)Image.FromFile(@"AlibataTrainingSet/" + allCharaters[i] + x + " (32x32) (8x8).jpg");
+                    tempImageCounter = 0;
+                    tempStr = "";
+                    for (int y = 0; y < tempImage.Width; y++)
+                    {
+                        for (int z = 0; z < tempImage.Height; z++)
+                        {
+                            allBinaryImages[i, x, tempImageCounter] = 0; //simulate black pixel
+                            if (tempImage.GetPixel(y, z).R > 200 && tempImage.GetPixel(y, z).G > 200 && tempImage.GetPixel(y, z).B > 200)
+                            {
+                                allBinaryImages[i, x, tempImageCounter] = 1; //simulate white pixel
+                            }
+                            tempStr += allBinaryImages[i, x, tempImageCounter] + " ";
+                            tempImageCounter++;
+                        }
+                        tempStr += "\n";
+                    }
+                    //MessageBox.Show(tempStr);
+                }
+            }
+        }
+
+        private void test_Click(object sender, EventArgs e)
+        {
+            Bitmap tempImage = drawerBitmap;
+            Bitmap resizedImage = resizeImage(ref tempImage, 8, 8);
+            resizedImage.Save("temp.bmp", ImageFormat.Bmp);
+            //drawer.Image = resizedImage;
+            int tempImageCounter = 0;
+            string tempStr = "";
+            Color tempColor;
+
+            for (int x=0; x<resizedImage.Width; x++)
+            {
+                for (int y=0; y<resizedImage.Height; y++)
+                {
+                    tempColor = resizedImage.GetPixel(x, y);
+                    //MessageBox.Show("At (" + x + "," + y + ")\nRGB (" + tempColor.R + "," + tempColor.G + "," + tempColor.B + ")");
+                    inputImage[tempImageCounter] = 0;
+                    if (tempColor.R > 200 && tempColor.G > 200 && tempColor.B > 200)
+                    {
+                        inputImage[tempImageCounter] = 1;
+                    }
+                    tempStr += inputImage[tempImageCounter] + " ";
+                    tempImageCounter++;
+                }
+                tempStr += "\n";
+            }
+
+            for (int x=0; x<inputImage.Length; x++)
+            {
+                bp.setInputs(x, inputImage[x]);
+            }
+            bp.run();
+            resultLabel.Text = Math.Round(bp.getOuputData(0), 4) + " " + Math.Round(bp.getOuputData(1), 4) + " " + Math.Round(bp.getOuputData(2), 4) + " " + Math.Round(bp.getOuputData(3), 4);
+            if ( Math.Round(bp.getOuputData(0)) == 0 && Math.Round(bp.getOuputData(1)) == 0 && Math.Round(bp.getOuputData(2)) == 0 && Math.Round(bp.getOuputData(3)) == 1)
+            {
+                resultPicBox.Image = (Bitmap)Image.FromFile(@"A.bmp");
+            }
+            else if (Math.Round(bp.getOuputData(0)) == 0 && Math.Round(bp.getOuputData(1)) == 0 && Math.Round(bp.getOuputData(2)) == 1 && Math.Round(bp.getOuputData(3)) == 0)
+            {
+                resultPicBox.Image = (Bitmap)Image.FromFile(@"E.bmp");
+            }
+            else if (Math.Round(bp.getOuputData(0)) == 0 && Math.Round(bp.getOuputData(1)) == 0 && Math.Round(bp.getOuputData(2)) == 1 && Math.Round(bp.getOuputData(3)) == 1)
+            {
+                resultPicBox.Image = (Bitmap)Image.FromFile(@"O.bmp");
+            }
+        }
+
+        public Bitmap resizeImage(ref Bitmap source, int width, int height)
+        {
+            Bitmap result = new Bitmap(width, height); //initalize the image
+            Color pixel;
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    pixel = source.GetPixel(x * source.Width / width, y * source.Height / height);
+                    result.SetPixel(x, y, pixel);
+                }
+            }
+            return result;
         }
 
         private void drawer_MouseDown(object sender, MouseEventArgs e)
@@ -40,12 +199,14 @@ namespace AlibataBackPropagation
             {
                 Graphics g;
                 g = Graphics.FromImage(drawerBitmap);
-                Pen myPen = new Pen(Color.Black);
                 System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
                 this.Cursor = new Cursor(Cursor.Current.Handle);
-                g.FillEllipse(myBrush, e.X, e.Y, brushWidth, brushWidth);
+                using (Graphics gl = Graphics.FromImage(drawerBitmap))
+                {
+                    gl.FillEllipse(myBrush, e.X, e.Y, brushWidth, brushWidth);
+                }
                 drawer.Image = drawerBitmap;
-                g.Dispose();
+                drawer.DrawToBitmap(drawerBitmap, new Rectangle(0, 0, drawer.Width, drawer.Height));
             }
         }
 
@@ -53,7 +214,6 @@ namespace AlibataBackPropagation
         {
             Graphics g;
             g = Graphics.FromImage(drawerBitmap);
-            Pen myPen = new Pen(Color.Black);
             System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.White);
             this.Cursor = new Cursor(Cursor.Current.Handle);
             g.FillRectangle(myBrush, 0, 0, drawer.Width, drawer.Height);
